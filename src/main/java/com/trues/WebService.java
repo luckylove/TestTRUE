c184e890a0dd6505b67b6eedc088f0b4400df79b
Cong Son <congson.nguyen@hrboss.com>
2014-09-18 17:54:31 +0700

    aaaa

--- src/main/java/com/trues/WebService.java
+++ src/main/java/com/trues/WebService.java
@@ -3,7 +3,10 @@ package com.trues;
 
 import com.trues.webservice.service.TRUEWService;
 import org.codehaus.jackson.map.ObjectMapper;
+import th.co.tit.ccbint.mcp.webservices.SearchAllCustomerProfile;
 import th.co.tit.ccbint.mcp.webservices.SearchTmvProfile;
+import th.co.tit.ccbint.mcp.webservices.SearchTruProfile;
+import th.co.tit.ccbint.mcp.webservices.SearchTvsProfile;
 
 import java.lang.reflect.Field;
 import java.util.HashMap;
@@ -24,6 +27,9 @@ public class WebService {
         Utils.clearConsole();
         System.out.println("===================     TRUE Web Service  =====================");
         System.out.println("===================     1. searchTmvProfile ");
+        System.out.println("===================     2. searchTruProfile ");
+        System.out.println("===================     3. searchTvsProfile ");
+        System.out.println("===================     4. searchAllCustomerProfile ");
 
 
 
@@ -31,15 +37,80 @@ public class WebService {
         String service = scanner.nextLine();
         if ("1".equals(service)) {
             searchTmvProfile(scanner);
+        } else if ("2".equals(service)) {
+            searchTruProfile(scanner);
+        }  else if ("3".equals(service)) {
+            searchTvsProfile(scanner);
+        }  else if ("4".equals(service)) {
+            searchTmvProfile(scanner);
         }  else {
             MainClass.mainMenu(scanner);
         }
+    }
+    public static void searchAllCustomerProfile(Scanner scanner) throws Exception {
+
+        Utils.clearConsole();
+        System.out.println("===================     4. searchAllCustomerProfile    =====================");
+
+        SearchAllCustomerProfile log = new SearchAllCustomerProfile();
+        Map<String, Object> input = new HashMap<String, Object>();
+        Field[] fields = log.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            System.out.println(String.format("Please input param (%s  - %s): ", field.getName(), Utils.getTypeName(field.getGenericType().toString())));
+            input.put(field.getName(), Utils.getNextNll(scanner, Utils.getTypeName(field.getGenericType().toString())));
+        }
+        Utils.map2Object(log, input);
+        TRUEWService.searchAllCustomerProfile(MainClass.SESSIONID, log);
+        System.out.println("Enter to next: ");
+        scanner.nextLine();
+        testWebService(scanner);
+
+    }
+
+    public static void searchTvsProfile(Scanner scanner) throws Exception {
+
+        Utils.clearConsole();
+        System.out.println("===================     3. searchTvsProfile    =====================");
+
+        SearchTvsProfile log = new SearchTvsProfile();
+        Map<String, Object> input = new HashMap<String, Object>();
+        Field[] fields = log.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            System.out.println(String.format("Please input param (%s  - %s): ", field.getName(), Utils.getTypeName(field.getGenericType().toString())));
+            input.put(field.getName(), Utils.getNextNll(scanner, Utils.getTypeName(field.getGenericType().toString())));
+        }
+        Utils.map2Object(log, input);
+        TRUEWService.searchTvsProfile(MainClass.SESSIONID, log);
+        System.out.println("Enter to next: ");
+        scanner.nextLine();
+        testWebService(scanner);
+
+    }
+
+    public static void searchTruProfile(Scanner scanner) throws Exception {
+
+        Utils.clearConsole();
+        System.out.println("===================     2. searchTruProfile   =====================");
+
+        SearchTruProfile log = new SearchTruProfile();
+        Map<String, Object> input = new HashMap<String, Object>();
+        Field[] fields = log.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            System.out.println(String.format("Please input param (%s  - %s): ", field.getName(), Utils.getTypeName(field.getGenericType().toString())));
+            input.put(field.getName(), Utils.getNextNll(scanner, Utils.getTypeName(field.getGenericType().toString())));
+        }
+        Utils.map2Object(log, input);
+        TRUEWService.searchTruProfile(MainClass.SESSIONID, log);
+        System.out.println("Enter to next: ");
+        scanner.nextLine();
+        testWebService(scanner);
+
     }
 
     public static void searchTmvProfile(Scanner scanner) throws Exception {
 
         Utils.clearConsole();
-        System.out.println("===================     22. Olympus_137MonoUnsub    =====================");
+        System.out.println("===================     1.searchTmvProfile    =====================");
 
         SearchTmvProfile log = new SearchTmvProfile();
         Map<String, Object> input = new HashMap<String, Object>();
